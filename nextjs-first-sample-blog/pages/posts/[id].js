da3287f42e5e492b958b1559b891476de9806209
Ian Mike <iammikeian@gmail.com>
2023-08-28 20:13:19 +0300

    :lipstick: Dynamic routes: Add styles to post pages

--- nextjs-first-sample-blog/pages/posts/[id].js
+++ nextjs-first-sample-blog/pages/posts/[id].js
@@ -14,9 +14,11 @@
  * 
  */
 
+import utilStyles from '../../styles/utils.module.css';
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
+import Date from '../../components/date';
 
 export async function getStaticPaths() {
     // return a list of possible value for id
@@ -43,13 +45,13 @@ export default function Post({ postData }) {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            <Date dateString={postData.date} />
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     );
 }
