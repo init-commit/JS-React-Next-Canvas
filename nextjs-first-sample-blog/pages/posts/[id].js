36968518c1e3d8264191678e9164c3c6437bf3fa
Ian Mike <iammikeian@gmail.com>
2023-08-28 19:57:48 +0300

    Dynamic routes: Polishing post page => adding date-fns lib & title. # fc6d536

--- nextjs-first-sample-blog/pages/posts/[id].js
+++ nextjs-first-sample-blog/pages/posts/[id].js
@@ -16,6 +16,7 @@
 
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
 
 export async function getStaticPaths() {
     // return a list of possible value for id
@@ -39,11 +40,14 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
     return (
         <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
             {postData.title}
             <br />
             {postData.id}
             <br />
-            {postData.date}
+            <Date dateString={postData.date} />
             <br />
             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
