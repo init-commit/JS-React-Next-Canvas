1ba5df781794d223661b0b51ccbbb144cf00875e
Ian Mike <iammikeian@gmail.com>
2023-08-28 20:16:11 +0300

    :sparkles: Add link to posts from home page

--- nextjs-first-sample-blog/pages/index.js
+++ nextjs-first-sample-blog/pages/index.js
@@ -3,6 +3,7 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -23,10 +24,6 @@ export default function Home({allPostsData}) {
         <p>
           Greetings!👋  I'm thrilled to welcome you to my corner of the digital realm. I'm a dedicated and curious junior developer embarking on an exciting journey through the ever-evolving landscape of coding and software development. <br /> <br />
           With a passion for crafting elegant solutions to complex problems, I find myself constantly captivated by the endless possibilities that the world of programming offers.</p>
-        <p>
-          More {' '}
-          <Link href="/posts/first-post">posts</Link>.
-        </p>
       </section>
 
       {/* new section to display fetched data */}
@@ -36,12 +33,12 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
           ))}
         </ul>
       </section>
